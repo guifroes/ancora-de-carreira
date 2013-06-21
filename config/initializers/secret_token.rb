a98beba649e51e9c471b1abf5f870332d8cbb5a2
guifroes <guilherme.froes@gmail.com>
2013-06-20 23:51:27 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AncoraDeCarreira::Application.config.secret_key_base = '55196f95f389cdb117bd4abb3970136959c07adc2e872c70d4f2a277cf75dbae7a4c912c7b86c34f55e5d3b91ed2307286cdccfbc8d4c5001efbf84b6050e162'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AncoraDeCarreira::Application.config.session_store :cookie_store, key: '_ancora-de-carreira_session'
