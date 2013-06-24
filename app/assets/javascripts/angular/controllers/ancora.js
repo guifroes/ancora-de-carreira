34ad2c741cbbfbbe4a4a200c209990e2ee1348ad
guifroes <guilherme.froes@gmail.com>
2013-06-24 19:23:39 -0300

    some dumb paging for the questions

--- app/assets/javascripts/angular/controllers/ancora.js
+++ app/assets/javascripts/angular/controllers/ancora.js
@@ -1,8 +1,14 @@
 'use strict';
 
-angular.module('ancora').controller('MainCtrl', ['$scope', '$http', function(scope, http) { 
+var app = angular.module('ancora').controller('MainCtrl', ['$scope', '$http', function(scope, http) { 
 
-  scope.steps = ['objective', 'inventory', 'instructions'];
+  scope.currentPage = 0;
+  scope.pageSize = 5;
+  scope.numberOfPages=function(){
+    return Math.ceil(scope.answer.questions.length/scope.pageSize);
+  }
+
+  scope.steps = ['objective', 'inventory', 'instructions', 'personal', 'questions1', 'questions2', 'questions3', 'questions4', 'questions5', 'questions6'];
   scope.step = 0;
 
   scope.isCurrentStep = function(step) {
@@ -54,8 +60,44 @@ angular.module('ancora').controller('MainCtrl', ['$scope', '$http', function(sco
 
   var questions = [{text: 'Sonho em ser tão bom no que faço de tal forma que meus conhecimentos especializados sejam constantemente procurados.', answer:{}},
                    {text: 'Sinto-me mais realizado em meu trabalho quando sou capaz de integrar e gerenciar o esforço dos outros.', answer:{}},
-                   {text: 'Sonho em ter uma carreira que me dê a liberdade de fazer o trabalho à minha maneira de no tempo por mim programado.', answer:{}}];
+                   {text: 'Sonho em ter uma carreira que me dê a liberdade de fazer o trabalho à minha maneira de no tempo por mim programado.', answer:{}},
+                   {text: "question 1", answer: {}},
+                   {text: "question 2", answer: {}},
+                   {text: "question 3", answer: {}},
+                   {text: "question 4", answer: {}},
+                   {text: "question 5", answer: {}},
+                   {text: "question 6", answer: {}},
+                   {text: "question 7", answer: {}},
+                   {text: "question 8", answer: {}},
+                   {text: "question 9", answer: {}},
+                   {text: "question 10", answer: {}},
+                   {text: "question 11", answer: {}},
+                   {text: "question 12", answer: {}},
+                   {text: "question 13", answer: {}},
+                   {text: "question 14", answer: {}},
+                   {text: "question 15", answer: {}},
+                   {text: "question 16", answer: {}},
+                   {text: "question 17", answer: {}},
+                   {text: "question 18", answer: {}},
+                   {text: "question 19", answer: {}},
+                   {text: "question 20", answer: {}},
+                   {text: "question 21", answer: {}},
+                   {text: "question 22", answer: {}},
+                   {text: "question 23", answer: {}},
+                   {text: "question 24", answer: {}},
+                   {text: "question 25", answer: {}},
+                   {text: "question 26", answer: {}},
+                   {text: "question 27", answer: {}},
+                   {text: "question 28", answer: {}},
+                   {text: "question 29", answer: {}},
+                   {text: "question 30", answer: {}},
+                   {text: "question 31", answer: {}}];
 
-  scope.answer = {name:"", email:"", questions: questions};
+  scope.answer = {name:"", email:"", institution: "", course: "", questions: questions};
 }]);
-
+app.filter('startFrom', function() {
+  return function(input, start) {
+    start = +start; //parse to int
+    return input.slice(start);
+  }
+});
