5bd89036ef0910e405e67972dae123e5f168a53a
guifroes <guilherme.froes@gmail.com>
2013-06-24 00:21:31 -0300

    trying some data and first page layout

--- app/assets/javascripts/angular/controllers/ancora.js
+++ app/assets/javascripts/angular/controllers/ancora.js
@@ -1,17 +1,16 @@
 'use strict';
 
 angular.module('ancora').controller('MainCtrl', ['$scope', '$http', function(scope, http) { 
-  scope.answer = {}; 
-  var postData = "{ \"answer\": { \"name\": \"jose\", \"email\": \"guilherme.froes@gmail.com\", \"answer_1\": \"4\", \"answer_2\": \"5\", \"answer_3\": \"10\", \"final_result\": \"bla bla\" } } ";
 
   scope.create = function() { 
+    var postData = "{ \"answer\": { \"name\": \""+ scope.answer.name +"\", \"email\": \""+ scope.answer.email+"\", \"answer_1\": \"" + scope.answer.questions[0].answer + "\", \"answer_2\": \"" + scope.answer.questions[1].answer+ "\", \"answer_3\": \"" + scope.answer.questions[2].answer + "\", \"final_result\": \"bla bla\" } } ";
     http.post("/api/answers/", postData);
   };
 
-  var questions = [{text: 'Sonho em ser tão bom no que faço de tal forma que meus conhecimentos especializados sejam constantemente procurados.'},
-                   {text: 'Sinto-me mais realizado em meu trabalho quando sou capaz de integrar e gerenciar o esforço dos outros.'},
-                   {text: 'Sonho em ter uma carreira que me dê a liberdade de fazer o trabalho à minha maneira de no tempo por mim programado.'}];
+  var questions = [{text: 'Sonho em ser tão bom no que faço de tal forma que meus conhecimentos especializados sejam constantemente procurados.', answer:{}},
+                   {text: 'Sinto-me mais realizado em meu trabalho quando sou capaz de integrar e gerenciar o esforço dos outros.', answer:{}},
+                   {text: 'Sonho em ter uma carreira que me dê a liberdade de fazer o trabalho à minha maneira de no tempo por mim programado.', answer:{}}];
 
-  scope.questions = questions;
+  scope.answer = {name:"", email:"", questions: questions};
 }]);
 
